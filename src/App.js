6a5eb226f3a5516cb81904fc7a0492d1e4f38fee
OmarFaruqueSabbir <avrosabbir18@gmail.com>
2022-04-29 16:05:52 +0600

    Banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/shared/NavBar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/home' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
