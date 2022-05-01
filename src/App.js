aeb3ca1c03bbecb7b71515df650fcc461a9b9380
OmarFaruqueSabbir <avrosabbir18@gmail.com>
2022-05-01 17:17:34 +0600

    Register page & Login page created, Register page authenticated with Firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home/Home';
 import ItemDetail from './components/Home/ItemDetail/ItemDetail';
 import ManageInventory from './components/Home/ManageInventory/ManageInventory';
+import LogIn from './LogIn/LogIn/LogIn';
+import Register from './LogIn/Register/Register';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path='/home' element={<Home />}/>
         <Route path='/item/:itemId' element={<ItemDetail />}></Route>
         <Route path='/manageInventory' element={<ManageInventory />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </div>
   );
---
+++ src/LogIn/SocialLogin/SocialLogin.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { FcGoogle } from 'react-icons/fc';
+import auth from '../../firebase.init';
+
+const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+
+    let errorElement;
+    if(error){
+        errorElement =
+        <p className='text-red-700 '>Error: {error?.message}
+        </p>
+    }
+    if(user){
+        navigate('/home')
+    }
+    return (
+        <>
+            <p className='text-indigo-700'>Or sign up with:</p>
+            {
+                errorElement
+            }
+            <button onClick={() => signInWithGoogle()} type="button" className="flex items-center justify-center h-12 px-6 w-full bg-indigo-600 mt-2 rounded font-semibold text-sm text-blue-100 hover:bg-indigo-700">
+                <FcGoogle style={{height:'30px'}} />
+                <span className='ps-2'>Google Sign In</span> 
+            </button>
+        </>
+    );
+};
+
+export default SocialLogin;
